e07259fc5a5319821a8834374858047ddee9a101
Yasser Shkeir <shkeiryasser@gmail.com>
2022-07-04 14:05:01 +0300

    Upload

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  completed: boolean = false;
+  taskList: any[] = [];
+  newTodoForm = this.formBuilder.group({
+    todoItem: '',
+    start: '',
+    end: '',
+  });
+
+  constructor(private formBuilder: UntypedFormBuilder) {}
+
+  addTask() {
+    const value = this.newTodoForm.value.todoItem;
+    const timeStart = this.newTodoForm.value.start;
+    const timeEnd = this.newTodoForm.value.end;
+    this.taskList.push({
+      id: this.taskList.length,
+      name: value,
+      timeBegin: timeStart,
+      timeDone: timeEnd,
+    });
+    window.localStorage.setItem('task', JSON.stringify(this.taskList));
+    this.newTodoForm.reset();
+  }
+
+  removeTask(i: any) {
+    this.taskList.splice(i, 1);
+    window.localStorage.setItem('task', JSON.stringify(this.taskList));
+  }
+
+  markDone(value: any) {
+    value.completed = !value.completed;
+    value.completed === true
+      ? this.taskList.push(
+          this.taskList.splice(this.taskList.indexOf(value), 1)[0]
+        )
+      : this.taskList.unshift(
+          this.taskList.splice(this.taskList.indexOf(value), 1)[0]
+        );
+  }
+
+  ngOnInit(): void {
+    this.taskList = window.localStorage.getItem('task')
+      ? JSON.parse(localStorage.getItem('task') || '{}')
+      : [];
+  }
+}
+function todoItem(todoItem: any) {
+  throw new Error('Function not implemented.');
+}
